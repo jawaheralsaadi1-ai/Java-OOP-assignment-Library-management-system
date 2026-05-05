c836af59eeb95e78f74d6f8ba7cc0c0b12effb38
jawaher <jawaheralsaadi!@gmail.com>
2026-05-05 14:11:49 +0400

    Add TO Menu.java//implement Menu to talks with user and calls the right Service

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import menu.Menu;
+import services.BorrowService;
+import services.LibraryService;
+import services.MemberService;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Setup Services (The Engines)
+        LibraryService libService = new LibraryService();
+        MemberService memService = new MemberService();
+
+        // 2. Setup Coordination (The Bridge)
+        BorrowService borrowService = new BorrowService(libService, memService);
+
+        // 3. Setup UI (The Dashboard)
+        Menu menu = new Menu(libService, memService, borrowService);
+
+        // 4. Start the engine!
+        menu.start();
+    }
+}
