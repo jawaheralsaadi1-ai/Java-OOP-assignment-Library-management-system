b5e96b95a8935e7af50f1ee97ea434add4057175
jawaher <jawaheralsaadi!@gmail.com>
2026-05-05 13:35:48 +0400

    Add Magazine.java// implement Magazine class and complete items inheritance hierarchy"

--- src/entities/Magazine.java
+++ src/entities/Magazine.java
@@ -1,4 +1,35 @@
 package entities;
 
-public class Magazine {
+/**
+ *
+ *  After implement abstract LibraryItem as a base
+ *  Just like the Book class, this Magazine extends LibraryItem.
+ It takes ID and Title from the LibraryItem but adds its own 'issueNumber'. */
+
+public class Magazine  extends LibraryItem {
+
+// "final" means: once we set them, they cannot change later
+    // Create variable store the  information which makes a Book unique
+    private final String issueNumber;
+
+    // Create the objects and store it values
+    public Magazine (String id, String title, String issueNumber ) {
+        // 'super' calls the Constructor of the  (LibraryItem)
+        super(id, title); // Calling to handle shared data
+        this.issueNumber = issueNumber;
+    }
+
+    /**
+     * Implementing the abstract method from LibraryItem.
+     * Now showing Magazine specific details.
+     */
+    @Override
+    public String getDetails() {
+        String status = isAvailable() ? "Available" : "Borrowed";
+        return String.format("Magazine [ID: %s] Title: %s | Issue: %s | Status: %s",
+                getId(), getTitle(), issueNumber, status);
+    }
+
+    // Getter for the unique field
+    public String getAuthor() { return issueNumber; }
 }
